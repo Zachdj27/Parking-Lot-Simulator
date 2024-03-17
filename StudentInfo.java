1ed0c518e5345b829248ab0e6eeaed0305869ea8
Zachary Dj <zachary@zacharys-mbp.home>
2024-03-17 13:53:00 -0400

    first commit

---
+++ StudentInfo.java
@@ -0,0 +1,29 @@
+/**
+* Contains a method <code>void display()</code> that all the <code>main</code>
+* methods call to show the student information. Fill the box with your personal
+* information.
+*
+* @author Marcel Turcotte (marcel.turcotte@uottawa.ca)
+*/
+
+public class StudentInfo {
+
+    /**
+    * Displays the student information: student name, id, section, etc for each
+    * member of the team.
+    */
+
+    public static void display() {
+
+        System.out.println("************************************************************");
+        System.out.println("*                                                          *");
+        System.out.println("*         Zachary Djerdjouri, 300355345, section B         *");
+        System.out.println("*                                                          *");
+        System.out.println("*         Youssuf Helaly, 300355994, section C             *");
+        System.out.println("*                                                          *");
+        System.out.println("************************************************************");
+        System.out.println();
+
+    }
+
+}
